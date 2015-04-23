11f85e065fd4a6a0214fc3eef3c99e33940c6842
kha le & courtney phillips <student@epicodus.com>
2015-04-23 16:01:29 -0700

    final

--- lib/coin_app.rb
+++ lib/coin_app.rb
@@ -3,47 +3,48 @@ require('sinatra/reloader')
 require('pry')
 
 
-  class Fixnum
-    define_method(:coin_app) do
-      change = self
-      quarters = []
-      qcounter = 0
-     until change. < (25)
-        change = change.-(25)
-        qcounter = qcounter.+(1)
-      end
-      quarters.push(qcounter)
+class Fixnum
+  define_method(:coin_app) do
+    change = self
+    quarters = ["quarters"]
+    qcounter = 0
+    until change. < (25)
+      change = change.-(25)
+      qcounter = qcounter.+(1)
+    end
+    quarters.unshift(qcounter)
 
     change = self.-(qcounter.*(25))
-      dimes = []
-      dcounter = 0
+    dimes = ["dimes"]
+    dcounter = 0
     until change. < (10)
       change = change.-(10)
       dcounter = dcounter.+(1)
     end
-    dimes.push(dcounter)
+    dimes.unshift(dcounter)
 
 
     change = self.-(qcounter.*(25))
     change = change.-(dcounter.*(10))
-    nickel = []
+    nickel = ["nickels"]
     ncounter = 0
     until change. < (5)
       change = change.-(5)
       ncounter = ncounter.+(1)
     end
-    nickel.push(ncounter)
+    nickel.unshift(ncounter)
 
     change = self.-(qcounter.*(25))
     change = change.-(dcounter.*(10))
     change = change.-(ncounter.*(5))
-    penny = []
+    penny = ["pennies"]
     pcounter = 0
     until change. < (1)
       change = change.-(1)
       pcounter = pcounter.+(1)
     end
-    penny.push(pcounter)
+    penny.unshift(pcounter)
     total = quarters + dimes + nickel + penny
+    total.join(" ")
   end
 end
